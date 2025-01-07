5b286cccdf3fc7fea3b652df33ffaf13f36a4d5e
dev.guillermoberbesi@gmail.com <dev.guillermoberbesi@gmail.com>
2025-01-07 18:26:24 -0400

    [feature] add fields avatar and phone to users

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,4 +1,8 @@
 <?php
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
@@ -7,13 +11,23 @@ test('registration screen can be rendered', function () {
 });
 
 test('new users can register', function () {
-    $response = $this->post('/register', [
+    Storage::fake('avatars');
+    $avatar = UploadedFile::fake()->image('avatar.jpg');
+    $response = $this->post('/register', $data = [
         'name' => 'Test User',
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
+        'phone' => fake()->phoneNumber(),
+        'avatar' => $avatar,
     ]);
 
     $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
+
+    $user = User::firstWhere('email', $data['email']);
+    $this->assertEquals($data['name'], $user->name);
+    $this->assertTrue(Hash::check($data['password'], $user->password));
+    $this->assertEquals($data['phone'], $user->phone);
+    Storage::disk('avatars')->assertExists($avatar->hashName());
 });
