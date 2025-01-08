d15888a24e7c38eb56604e4511bb2edbf2b5fda3
dev.guillermoberbesi@gmail.com <dev.guillermoberbesi@gmail.com>
2025-01-07 20:34:18 -0400

    refactoring

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use App\Models\User;
+use App\Jobs\SendEmailForUsersDisconnectedJob
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::job(new \App\Jobs\SendEmailForUsersDisconnectedJob())->daily();
+Schedule::job(new SendEmailForUsersDisconnectedJob())->daily()
+    ->when(fn() => User::whereDate('latest_login','<=',now()->subDays(30)->format('Y-m-d'))->exists() );
